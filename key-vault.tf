b72554ec6a3b5993c0a0ec26717f87be7f3a5c79
al-hmcts <166380454+al-hmcts@users.noreply.github.com>
2026-08-10 11:16:38 +0100

    Add Azure Key Vault data sources and secret resource
    Added data sources for Azure Key Vault S2S secret.

--- key-vault.tf
+++ key-vault.tf
@@ -17,6 +17,27 @@ module "finrem-vault" {
   common_tags             = local.tags
 }
 
+data "azurerm_key_vault" "key_vault" {
+  name                = "$finrem-${var.env}"
+  resource_group_name = "finrem-${var.env}"
+}
+
+data "azurerm_key_vault" "s2s_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "key_from_vault" {
+  name         = "microservicekey-finrem-citizen-ui"
+  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+}
+
+resource "azurerm_key_vault_secret" "s2s" {
+  name         = "finrem-citizen-s2s-client-secret"
+  value        = data.azurerm_key_vault_secret.key_from_vault.value
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 output "vaultName" {
   value = module.finrem-vault.key_vault_name
 }
